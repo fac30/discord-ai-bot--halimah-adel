c539df6e87f8ed2b4431e49c8329d48a02495b2e
Leda909 <ko9zad@gmail.com>
2024-02-25 19:33:31 +0000

    move openAI request to function

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,35 @@
 const { Events, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js');
 const { OpenAI } = require('openai');
 const client = require("../handlers/newClient");
-const fetchHistory = require("../handlers/fetchHistory")
+const fetchHistory = require("../handlers/fetchHistory");
+
 
 // Use API Key Directly
 const openai = new OpenAI({ apiKey: process.env.API_KEY });
+
+const openApiRequirest = async (msg) => {
+    try{
+        // Type effect while bot fetch respond from openAI
+        await msg.channel.sendTyping();
+
+        const conversation = await fetchHistory(msg);
+
+        // Post prompt and Get openAI response
+        const chatCompletion = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo-1106',
+            messages: conversation,
+            max_tokens: 30,
+        });
+
+        const response = chatCompletion.choices[0].message.content;
+        // console.log(JSON.stringify(chatCompletion, null, 2));
+        return { response };
+        
+    } catch (error) {
+            console.error('openAI Request Error:', error);
+    }
+}
+
 
 module.exports = {
     name: Events.MessageCreate,
@@ -20,24 +45,25 @@ module.exports = {
         // Ignore message if it doesn't start with !
         if (!msg.content.startsWith('!')) return;
 
-        // Type effect for bot
-        await msg.channel.sendTyping();
+        
       
             
         // Connect to OpenAI with Error Handling to recieve respond for prompt
         try {
-            const conversation = await fetchHistory(msg);
-            // const isPrivate = msg.content.startsWith('!private');
+            // const conversation = await fetchHistory(msg);
+
             
-            const chatCompletion = await openai.chat.completions.create({
-                model: 'gpt-3.5-turbo-1106',
-                messages: conversation,
-                max_tokens: 25,
-            });
+            // Fetch openAI request
+            // const chatCompletion = await openai.chat.completions.create({
+            //     model: 'gpt-3.5-turbo-1106',
+            //     messages: conversation,
+            //     max_tokens: 25,
+            // });
     
-            const response = chatCompletion.choices[0].message.content;
+            // const response = chatCompletion.choices[0].message.content;
             // console.log('OpenAI Response:', response);
             // console.log(JSON.stringify(chatCompletion, null, 2));
+            const { response } = await openApiRequirest(msg);
             
             // Direct message the openAI respond to the user with the user prompt which will be removed 
             if (msg.content.startsWith('!private')) {
@@ -49,15 +75,10 @@ module.exports = {
                         .catch(console.error);
                     // Delete the original message
                     await msg.delete();
-                    
+                    await msg.reply(`Your private message has been processed.`);
                 }
                 catch (error) {
-                    console.error('Error fetching user or sending direct message:', error);
-                }
-                try {
-                    await msg.channel.send(`Your private message has been processed.`)
-                } catch (error) {
-                    console.error('Error confirming direct message:', error); 
+                    console.error('Error during "!private" Direct Message, bc fetching user or dm or confirm dm.', error);
                 }
             }
             //Add Buttons to openAI respond in every prompt
