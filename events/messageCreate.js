dfc15ef22ad56f43940d4679d6237808cf5a98b3
Leda909 <ko9zad@gmail.com>
2024-02-25 21:32:21 +0000

    pull ChatGDP fetch to another folder

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,34 +1,6 @@
 const { Events, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js');
-const { OpenAI } = require('openai');
 const client = require("../handlers/newClient");
-const fetchHistory = require("../handlers/fetchHistory");
-
-
-// Use API Key Directly
-const openai = new OpenAI({ apiKey: process.env.API_KEY });
-
-const openApiRequirest = async (msg) => {
-    try{
-        // Type effect while bot fetch respond from openAI
-        await msg.channel.sendTyping();
-
-        const conversation = await fetchHistory(msg);
-
-        // Post prompt and Get openAI response
-        const chatCompletion = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo-1106',
-            messages: conversation,
-            max_tokens: 30,
-        });
-
-        const response = chatCompletion.choices[0].message.content;
-        // console.log(JSON.stringify(chatCompletion, null, 2));
-        return { response };
-        
-    } catch (error) {
-            console.error('openAI Request Error:', error);
-    }
-}
+const openApiRequest = require("../handlers/openApiRequest");
 
 
 module.exports = {
@@ -46,33 +18,18 @@ module.exports = {
         if (!msg.content.startsWith('!')) return;
 
         
-      
-            
         // Connect to OpenAI with Error Handling to recieve respond for prompt
         try {
-            // const conversation = await fetchHistory(msg);
-
-            
-            // Fetch openAI request
-            // const chatCompletion = await openai.chat.completions.create({
-            //     model: 'gpt-3.5-turbo-1106',
-            //     messages: conversation,
-            //     max_tokens: 25,
-            // });
-    
-            // const response = chatCompletion.choices[0].message.content;
-            // console.log('OpenAI Response:', response);
-            // console.log(JSON.stringify(chatCompletion, null, 2));
-            const { response } = await openApiRequirest(msg);
+            // import openAIRequest with fetchHistory of last 10 messages
+            const { response } = await openApiRequest(msg);
             
             // Direct message the openAI respond to the user with the user prompt which will be removed 
             if (msg.content.startsWith('!private')) {
                 try {
                     const user = await client.users.fetch(msg.author.id);
-                    //console.log(`userName: ${user}`);
+                    
                     user.send(`In response to your message: "${msg.content}", the AI says: "${response}"`)
-                        //.then(msg => console.log(`Sent message: ${msg.content} to ${user}`))
-                        .catch(console.error);
+                  
                     // Delete the original message
                     await msg.delete();
                     await msg.reply(`Your private message has been processed.`);
@@ -108,8 +65,7 @@ module.exports = {
             }
         }
         catch (error) {
-            console.error('Conversation history error:', error);
-            console.error('OpenAI Error:', error);
+            console.error('Error during openAI Bot response', error);
             msg.reply('An error occurred while processing your request. Please try again later.');
         }  
     }
